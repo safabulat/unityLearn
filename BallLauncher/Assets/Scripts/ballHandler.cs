a1d3cc78349d56da3c4c184d89a1e9a7c8029ec5
Safa <safa.bulat2014@gtu.edu.tr>
2022-09-05 15:13:12 +0300

    End of the section.
    Added:
    Multitouch, ScreenFit, Lauch Ball, Re-Spawn ball, Detach Ball etc.
    Might Add:
    Delete previous spawns,  score, menu (quit play etc), couple of new levels.

--- BallLauncher/Assets/Scripts/ballHandler.cs
+++ BallLauncher/Assets/Scripts/ballHandler.cs
@@ -2,25 +2,102 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.EnhancedTouch;
+using Touch = UnityEngine.InputSystem.EnhancedTouch.Touch;
 
 public class ballHandler : MonoBehaviour
 {
+    //Inspector Properities
+    [SerializeField] private GameObject ballPrefab;
+    [SerializeField] private Rigidbody2D pivot;
+    [SerializeField] private float detachDelay;
+    [SerializeField] private float respawnDelay;
+    //Private declerations
+    private Rigidbody2D currentBallRigidBody;
+    private SpringJoint2D currentBallSpringJoint;
+    private bool isDragging;
+    private Camera m_Camera;
     // Start is called before the first frame update
     void Start()
     {
-        
+        m_Camera = Camera.main;
+        SpawnNewBall();
+    }
+
+    void OnEnable()
+    {
+        EnhancedTouchSupport.Enable();
+    }
+
+    void OnDisable()
+    {
+        EnhancedTouchSupport.Disable();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!Touchscreen.current.primaryTouch.press.isPressed)
+        if(currentBallRigidBody == null) {  return; }
+
+        if (Touch.activeTouches.Count == 0) //one touch -> Touchscreen.current.primaryTouch.press.isPressed
         {
+            if (isDragging)
+            {
+                LaunchBall();
+                
+            }
+            isDragging = false;
+            
             return;
         }
-        Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
 
-        Debug.Log(touchPosition);
+        isDragging = true;
 
+        currentBallRigidBody.isKinematic = true;
+
+        //For single touch - > Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+        //Multi Touch;
+        Vector2 touchPos = new Vector2();
+
+        foreach(Touch touch in Touch.activeTouches)
+        {
+            touchPos += touch.screenPosition;
+        }
+
+        touchPos /= Touch.activeTouches.Count;
+
+        Vector3 worldPosition = m_Camera.ScreenToWorldPoint(touchPos);
+        
+        currentBallRigidBody.position = worldPosition;    
+        
+
+        Debug.Log("touch pos: " + touchPos + "\tworld pos: " + worldPosition);
+    }
+
+    private void SpawnNewBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, pivot.position, Quaternion.identity);
+
+        currentBallRigidBody = ballInstance.GetComponent<Rigidbody2D>();
+        currentBallSpringJoint = ballInstance.GetComponent<SpringJoint2D>();
+
+        currentBallSpringJoint.connectedBody = pivot;
+
+    }
+
+    private void LaunchBall()
+    {
+        currentBallRigidBody.isKinematic = false;
+        currentBallRigidBody = null;
+
+        Invoke(nameof(DetachBall), detachDelay);
+    }
+
+    private void DetachBall()
+    {
+        currentBallSpringJoint.enabled = false;
+        currentBallSpringJoint = null;
+
+        Invoke(nameof(SpawnNewBall), respawnDelay);
     }
 }
